3f70195856b7a46f5cd481bbcb5fc3b59e144eb0
xin-huang <xinhuang.res@gmail.com>
2025-11-07 21:04:16 +0100

    feat: add extra-trees classifier

--- gaishi/models/etc_model.py
+++ gaishi/models/etc_model.py
@@ -1,5 +1,6 @@
+# Copyright 2025 Xin Huang
+#
 # GNU General Public License v3.0
-# Copyright 2024 Xin Huang
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,8 +16,3 @@
 # along with this program. If not, please see
 #
 #    https://www.gnu.org/licenses/gpl-3.0.en.html
-
-
-from .ml_model import MlModel
-from .lr_model import LrModel
-from .etc_model import EtcModel
