872650fd3955f56d89e8cdcbc5543fc9db3a9c78
Xin Huang <xin-huang@users.noreply.github.com>
2026-05-08 01:27:24 +0200

    Unify test file GPL headers to 2026 template

--- tests/preprocessors/test_genotype_matrix_preprocessor.py
+++ tests/preprocessors/test_genotype_matrix_preprocessor.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Xin Huang
+# Copyright 2026 Xin Huang
 #
 # GNU General Public License v3.0
 #
@@ -16,7 +16,6 @@
 # along with this program. If not, please see
 #
 #    https://www.gnu.org/licenses/gpl-3.0.en.html
-
 
 import pytest
 from gaishi.generators import PolymorphismDataGenerator
